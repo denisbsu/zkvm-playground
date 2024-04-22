90479c64de167cd9fe9463dd0d22cc7b635eeb9a
Denis Kanonik <denisbsu@gmail.com>
2024-04-22 23:40:51 +0300

    Beautify

--- jolt-arc4/src/main.rs
+++ jolt-arc4/src/main.rs
@@ -3,25 +3,31 @@ use jolt::Proof;
 pub fn main() {
     let (prove_rc4, verify_rc4) = guest::build_rc4();
     use std::time::Instant;
+    let iterations = 10;
+    let mut proofs = Vec::<Proof>::new();
     let now = Instant::now();
-    let iterations = 1;
-    // let mut proof = Proof{proof};
-    // let mut output: u128 = 0;
-    // for _ in 0..iterations {
-        let mut text = [0u8; 32];
+    for _ in 0..iterations {
+        let text = [0u8; 32];
         let key = [0u8, 1, 2, 3, 4, 5, 6, 7];
-        let (output, proof) = prove_rc4(text, key);
-    // }
+        let (_, proof) = prove_rc4(text, key);
+        proofs.push(proof);
+    }
     let elapsed_prove = now.elapsed();
-    // proof.save_to_file("proof.bin").unwrap();
-    let is_valid = verify_rc4(proof);
+
+    let text = [0u8; 32];
+    let key = [0u8, 1, 2, 3, 4, 5, 6, 7];
+    let (output, proof) = prove_rc4(text, key);
+    proof.save_to_file("proof.bin").unwrap();
+    let now = Instant::now();
+    for _ in 0..iterations {
+        _ = verify_rc4(proofs.pop().unwrap());
+    }
     let elapsed_verify = now.elapsed();
 
 
-    // println!("output: {:?}", output);
-    println!("valid: {}", is_valid);
+    println!("output: {:?}", output);
     println!("Elapsed prove: {:.2?}", elapsed_prove / iterations);
-    println!("Elapsed verify: {:.2?}", elapsed_verify - elapsed_prove);
+    println!("Elapsed verify: {:.2?}", elapsed_verify / iterations);
 
     
 }
--- jolt-collatz/src/main.rs
+++ jolt-collatz/src/main.rs
@@ -3,22 +3,29 @@ use jolt::Proof;
 pub fn main() {
     let (prove_collatz, verify_collatz) = guest::build_collatz_bench();
     use std::time::Instant;
+    let iterations = 10;
     let now = Instant::now();
-    // let mut proof = Proof{proof};
-    let mut output: u128 = 0;
-    for _ in 0..10 {
-        let (output, proof) = prove_collatz(931_386_509_544_713_451, 10);
+
+    let mut proofs = Vec::<Proof>::new();
+    for _ in 0..iterations {
+        let (_, proof) = prove_collatz(931_386_509_544_713_451, 10);
+        proofs.push(proof);
     }
     let elapsed_prove = now.elapsed();
-    // proof.save_to_file("proof.bin").unwrap();
-    // let is_valid = verify_collatz(proof);
+
+    let (output, proof) = prove_collatz(931_386_509_544_713_451, 10);
+    proof.save_to_file("proof.bin").unwrap();
+
+    let now = Instant::now();
+    for _ in 0..iterations {
+        _ = verify_collatz(proofs.pop().unwrap());
+    }
     let elapsed_verify = now.elapsed();
 
 
-    // println!("output: {}", output);
-    // println!("valid: {}", is_valid);
-    println!("Elapsed prove: {:.2?}", elapsed_prove / 10);
-    println!("Elapsed verify: {:.2?}", elapsed_verify - elapsed_prove);
+    println!("output: {}", output);
+    println!("Elapsed prove: {:.2?}", elapsed_prove / iterations);
+    println!("Elapsed verify: {:.2?}", elapsed_verify / iterations);
 
     
 }
